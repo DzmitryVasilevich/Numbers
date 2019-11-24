91bfe84637111e971c043c29bc9225f98509aa07
Dzmitry Vasilevich <dzmitry_vasilevich@epam.com>
2019-11-24 18:11:52 +0300

    Creating a new project Numbers

---
+++ Project_Numbers/Project_Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Project_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    string output = String.Format("{0} {1}",
+                               i, " FizzBuzz");
+                    sb.AppendLine(output);
+                }
+                else if (i % 5 == 0)
+                {
+                    string output = String.Format("{0} {1}",
+                              i, " Buzz");
+                    sb.AppendLine(output);
+                }
+                else if (i % 3 == 0)
+                {
+                    string output = String.Format("{0} {1}",
+                             i, " Fizz");
+                    sb.AppendLine(output);
+                }
+                else
+                {
+                    sb.AppendLine(i.ToString());
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
