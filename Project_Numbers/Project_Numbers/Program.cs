b548f4210fda297c67eb20260047ce58ed70b031
Dzmitry Vasilevich <dzmitry_vasilevich@epam.com>
2019-11-25 11:45:21 +0300

    Remove hardcode values for Words and Multiplicity Numbers

--- Project_Numbers/Project_Numbers/Program.cs
+++ Project_Numbers/Project_Numbers/Program.cs
@@ -7,22 +7,42 @@ namespace Project_Numbers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is multiplicity for the 1st word?");
+            var multiplicity1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is the 1st word?");
+            var word1 = Console.ReadLine();
+
+
+            Console.WriteLine("What is multiplicity for the 2nd word?");
+            var multiplicity2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is the 2nd word?");
+            var word2 = Console.ReadLine();
+
+
+            Console.WriteLine("What is multiplicity for the 3rd word?");
+            var multiplicity3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is the 3rd word?");
+            var word3 = Console.ReadLine();
+
             var sb = new StringBuilder();
             for (int i = 0; i <= 100; i++)
-            {
-                if (i % 3 == 0 && i % 5 == 0)
+            {                
+                if (i % multiplicity1 == 0)
                 {
-                    string output = String.Format($"{i} FizzBuzz");
+                    string output = String.Format($"{i} {word1}");
                     sb.AppendLine(output);
                 }
-                else if (i % 5 == 0)
+                else if (i % multiplicity2 == 0)
                 {
-                    string output = String.Format($"{i} Buzz");
+                    string output = String.Format($"{i} {word2}");
                     sb.AppendLine(output);
                 }
-                else if (i % 3 == 0)
+                else if (i % multiplicity3 == 0)
                 {
-                    string output = String.Format($"{i} Fizz");
+                    string output = String.Format($"{i} {word3}");
                     sb.AppendLine(output);
                 }
                 else
