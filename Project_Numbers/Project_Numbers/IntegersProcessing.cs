12ec13501e4b7a4d812de41c19d1c362a893aad6
Dzmitry Vasilevich <dzmitry_vasilevich@epam.com>
2019-11-27 17:55:16 +0300

    Using Generics

---
+++ Project_Numbers/Project_Numbers/IntegersProcessing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Numbers
+{
+   public static class IntegersProcessing
+    {
+        public static List<string> RecieveNumbers(Dictionary<int, string> numericsAndWords)
+        {
+            var result = new List<string>();
+            for (int i = 1; i <= 100; i++)
+            {
+                foreach (var item in numericsAndWords)
+                {
+                    if (i % item.Key == 0)
+                    {
+                        result.Add($"{i}{item.Value}");
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
--- Project_Numbers/Project_Numbers/Program.cs
+++ Project_Numbers/Project_Numbers/Program.cs
@@ -1,57 +1,21 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Project_Numbers
-{
+{    
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            Console.WriteLine("What is multiplicity for the 1st word?");
-            var multiplicity1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("What is the 1st word?");
-            var word1 = Console.ReadLine();
-
-
-            Console.WriteLine("What is multiplicity for the 2nd word?");
-            var multiplicity2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("What is the 2nd word?");
-            var word2 = Console.ReadLine();
-
-
-            Console.WriteLine("What is multiplicity for the 3rd word?");
-            var multiplicity3 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("What is the 3rd word?");
-            var word3 = Console.ReadLine();
-
-            var sb = new StringBuilder();
-            for (int i = 0; i <= 100; i++)
+            var dict = new Dictionary<int, string>();
+            
+            var r = IntegersProcessing.RecieveNumbers(dict);
+            foreach (var item in r)
             {
-                if (i % multiplicity1 == 0)
-                {
-                    string output = $"{i} {word1}";
-                    sb.AppendLine(output);
-                }
-                else if (i % multiplicity2 == 0)
-                {
-                    string output = $"{i} {word2}";
-                    sb.AppendLine(output);
-                }
-                else if (i % multiplicity3 == 0)
-                {
-                    string output = $"{i} {word3}";
-                    sb.AppendLine(output);
-                }
-                else
-                {
-                    sb.AppendLine(i.ToString());
-                }
+                Console.WriteLine(item);
             }
-            Console.WriteLine(sb.ToString());
-        }
+        } 
     }
 }
